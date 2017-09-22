557573bd3c8e18f51c3476dbc2c1f0ad1df44ecf
Fiegellan <cfiegel@horizon.csueastbay.edu>
2017-09-21 18:51:01 -0700

    Changing the stream function
    Update the streaming and including the insert

--- streaming.js
+++ streaming.js
@@ -1,5 +1,9 @@
 const $ = require('jquery')  // jQuery now loaded and assigned to $
-const Twitter = require('twitter');
+const Twitter = require('twitter')
+const Stream = require('stream')
+
+
+const items = ['Twitter Test','hello']
 
 const twitterClient = new Twitter({
 
@@ -7,10 +11,26 @@ const twitterClient = new Twitter({
 
 var streaming = twitterClient.stream('statuses/filter', {track: 'Autodesk'});
 streaming.on('data', function(event) {
-  $('#tweets').text(event.text);
-  console.log(event && event.text);
+  #skelton of how to look
+  #assign to ID user name
+
+  items.push(event.text + ' -  ' + event.user.screen_name);
+
+  console.log(items);
+  var c = items.length-1;
+#give ids and classes
+  for (var c = items.length-1; c < items.length; c++) {
+      var newElement = document.createElement('div');
+      newElement.id = items[c];
+      newElement.innerHTML = items[c];
+      document.body.appendChild(newElement);
+  }
+
+
+  //$('#tweets').text(items);
+
 });
 
-  streaming.on('error', function(error) {
+streaming.on('error', function(error) {
     throw error;
   });
